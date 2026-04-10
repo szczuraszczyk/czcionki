16704bba81c2448aa55cd8b89f3194c7de024243
szczuraszczyk <ameli.szczelaszczyk@gmail.com>
2026-04-10 09:41:51 +0200

    Dodaj pliki projektów.

---
+++ czcionki/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "czcionki")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "czcionki.Pages")]
---
+++ czcionki/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace czcionki
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> Items { get; set; } = new() { "Dzień dobry", "Good morning", "Buenos Dias" };
+
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+
+        }
+    }
+}
